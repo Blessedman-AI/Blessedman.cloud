d9ad8e81795ecedfe4b6764dbc4fd6088af6cb53
Blessedman-AI <beemhanalabo@gmail.com>
2024-03-18 10:16:12 +0100

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,16 @@
 "use client";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function Homepage() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth <= 768) {
+      setIsMobile(true);
+    }
+  });
   return (
     <motion.div
       className="h-[calc(100vh-6rem)]  item-center justify-center
@@ -12,21 +20,24 @@ export default function Homepage() {
       transition={{ duration: 1 }}
     >
       <div
-        className="overflow-hidden spl-4 gap-12 flex flex-col md:flex-row h-full  w-full  min-h-full
-       items-center justify-around"
+        className="overflow-hidden pl-4 gap-12 flex flex-col
+         md:flex-row h-full  w-full  min-h-full items-center justify-around"
       >
         {/* IMAGE CONTAINER */}{" "}
-        <div className="h-[30%] w-2/3 items-center justify-center flex flex-1 ">
+        <div
+          className=" flex-shrink h-[30%] w-2/3 items-center justify-center
+         flex flex-1 pt-12 "
+        >
           <Image
             src="/hero.png"
             alt=""
-            width={500}
-            height={500}
+            width={isMobile ? 350 : 500}
+            height={isMobile ? 250 : 500}
             loading="lazy"
           />
         </div>
         {/* TEXT CONTAINER */}
-        <div className="flex flex-col max-w-[800px]  flex-1 pr-14">
+        <div className="flex-shrink pt-8 flex flex-col max-w-[800px]  flex-1 pr-14">
           <h1 className="text-4xl md:text-6xl font-bold">
             Crafting Digital Experiences, Designing Tomorrow.
           </h1>
@@ -38,7 +49,7 @@ export default function Homepage() {
           </p>
 
           {/* BUTTONS */}
-          <div className="pb-16 md:pb-0 h-full pt-6 w-full flex gap-4">
+          <div className="pb-6 md:pb-0 h-full pt-6 w-full flex gap-4">
             <button
               className="p-4 rounded-lg ring-1 ring-black
            bg-black text-white"
