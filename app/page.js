79c6680b826245df44417fe4026b50802b24c395
Blessedman-AI <beemhanalabo@gmail.com>
2024-06-11 13:01:22 +0100

    updated bio

--- app/page.js
+++ app/page.js
@@ -17,12 +17,9 @@ export default function Homepage() {
           <WavyText text="Welcome" fontSize="text-5xl lg:text-7xl" />
         }
         desc1="My name is Blessedman Igbedion. A Software 
-        Engineer with a strong  focus on JavaScript and related technologies 
-        and a proven track record in developing and deploying robust production-grade
-        applications."
-        desc2="I'm passionate about problem-solving, 
-         optimising performance, and delivering high-quality, 
-         scalable solutions."
+        Engineer with a strong  focus on JavaScript and related technologies."
+        desc2="I'm passionate about problem-solving - developing and deploying robust production-grade
+        applications, optimising performance, and delivering high-quality, scalable solutions."
       />
     </section>
   );
