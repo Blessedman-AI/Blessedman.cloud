d26a552870963762aa72245f0e27bb81506d99a0
Blessedman-AI <beemhanalabo@gmail.com>
2025-03-08 08:31:22 +0100

    removed some items

--- app/data/projects.jsx
+++ app/data/projects.jsx
@@ -19,21 +19,4 @@ export const items = [
     img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     link: `/konverse`,
   },
-
-  {
-    id: 5,
-    color: "from-red-200 to-red-200",
-    title: "PeekTure",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/16764124/pexels-photo-16764124/free-photo-of-programmer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    link: `/peekture`,
-  },
-
-  {
-    id: 1,
-    title: "MazaMaza",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    link: `/mazamaza`,
-  },
 ];
